cce6e8547f34576b6c6a1c2d7016662c41702d1a
Tonomatic <antonysoly2217@gmail.com>
2021-09-22 10:58:24 -0400

    almost done

--- index.js
+++ index.js
@@ -2,15 +2,27 @@ document.addEventListener("DOMContentLoaded", () => {
 
     const left = document.getElementById('left')
     const right = document.getElementById('right')
+    const location = document.getElementById('location')
 
-    const arr = ['app', 'blog', 'shop', 'note', 'grave']
-
+    const arr = ['app', 'blog', 'shop', 'note', 'game', 'grave']
+    let i = 0;
+    location.innerHTML = ` ${arr[i]}`
+    location.style.color = "rgb(46, 217, 235)"
     left.addEventListener("click", (e) => {
-        console.dir(e.target)
-        console.log(`you clicked this ${e}`)
+        if (i === 0) {
+            i = arr.length - 1;
+        } else {
+            i--;
+        }
+        location.innerHTML = ` ${arr[i]}`
     })
 
     right.addEventListener("click", (e) => {
-        console.log(`you clicked this ${e}`)
+        if (i === arr.length - 1) {
+            i = 0;
+        } else {
+            i++;
+        }
+        location.innerHTML = ` ${arr[i]}`
     })
 })
