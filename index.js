109abcfd91c77ab4d11d4dcee772851c88d4332f
Tonomatic <antonysoly2217@gmail.com>
2021-09-22 09:59:23 -0400

    so far

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-    const left = document.getElementById('#left')
-    const right = document.getElementById('#right')
+    const left = document.getElementById('left')
+    const right = document.getElementById('right')
 
+    const arr = ['app', 'blog', 'shop', 'note', 'grave']
+
+    left.addEventListener("click", (e) => {
+        console.dir(e.target)
+        console.log(`you clicked this ${e}`)
+    })
+
+    right.addEventListener("click", (e) => {
+        console.log(`you clicked this ${e}`)
+    })
 })
